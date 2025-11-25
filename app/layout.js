38ab42042854791c87e7cb98b0b6d09656d1847a
Tochukwu Ozo-Osiedo <tochukwuosiedo001@gmail.com>
2025-11-25 15:51:07 +0100

    nav done

--- app/layout.js
+++ app/layout.js
@@ -1,15 +1,10 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Montserrat } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const montserrat = Montserrat({
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
 });
 
 export const metadata = {
@@ -20,10 +15,8 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <Navbar/>
+      <body className={`antialiased ${montserrat.className}`}>
+        <Navbar />
         {children}
       </body>
     </html>
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,7 +1,16 @@
+"use client";
 import Image from "next/image";
 import Link from "next/link";
+import { LuUserRoundPen } from "react-icons/lu";
+import { IoIosMenu, IoMdClose } from "react-icons/io";
+import { useState } from "react";
+
 
 export default function Navbar() {
+
+    const [navOpen, setNavOpen] = useState(false)
+    console.log(navOpen);
+
 
     const navItems = [
         {
@@ -23,20 +32,40 @@ export default function Navbar() {
     ]
 
     return (
-        <main className="p-5 shadow-md flex items-center justify-between">
-            <Link href={'/'} className="flex items-center gap-1">
+        <main className="p-5 shadow-md flex items-center justify-between relative">
+            <Link href={'/'} className="flex items-center gap-1 z-40">
                 <Image src={"/logo.png"} alt="logo" width={500} height={500} className="w-7 h-7" />
 
-                <p className="text-xl">CodeCache</p>
+                <p className="text-xl max-md:hidden">CodeCache</p>
             </Link>
 
-            <div className="flex items-center gap-10">
+            <div className="flex items-center gap-7 max-lg:hidden">
                 {
                     navItems.map((item, index) => (
-                        <Link key={index} href={item.url} className="text-xl hover:text-amber-600">{item.label}</Link>
+                        <Link key={index} href={item.url} className="text-lg hover:text-amber-600">{item.label}</Link>
                     ))
                 }
             </div>
+
+            <Link href={"/auth/signin"} className="flex items-center gap-1 text-lg hover:text-amber-600 max-lg:ml-auto z-40">
+                <p className="max-md:hidden">Sign In</p>
+                <LuUserRoundPen />
+            </Link>
+
+            {/* mobile and tablet navbar view */}
+            <div className={`h-dvh bg-white w-full lg:hidden absolute top-0 right-0 flex-col flex gap-12 items-center pt-20 ${navOpen ? "block" : "hidden"}`}>
+                {
+                    navItems.map((item, index) => (
+                        <Link key={index} href={item.url} className="text-lg hover:text-amber-600">{item.label}</Link>
+                    ))
+                }
+            </div>
+
+            <button onClick={() => setNavOpen(!navOpen)} className="lg:hidden text-xl ml-5 z-40">
+                {
+                    navOpen ? <IoMdClose /> : <IoIosMenu />
+                }
+            </button>
         </main>
     )
 }

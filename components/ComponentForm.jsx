8d40ee2508b651f46b63b12a11bbf96eee42554c
Tochukwu Ozo-Osiedo <tochukwuosiedo001@gmail.com>
2025-12-15 15:39:36 +0100

    updates

--- components/ComponentForm.jsx
+++ components/ComponentForm.jsx
@@ -1,12 +1,32 @@
 "use client";
-import React from 'react'
+import React, { useState } from 'react'
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import { Button } from '@mui/material';
 import * as Yup from 'yup';
 import { collection, addDoc } from "firebase/firestore";
 import { db } from '@/config/firebase';
+import { TbLoader3 } from "react-icons/tb";
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
+import Box from '@mui/material/Box';
+
+const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    boxShadow: 24,
+    p: 4,
+};
 
 const ComponentForm = ({ session }) => {
+    const [open, setOpen] = useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+
+    const [processing, setProcessing] = useState(false)
 
     const formValidation = Yup.object({
         name: Yup.string().required("Please enter the component's name"),
@@ -26,8 +46,9 @@ const ComponentForm = ({ session }) => {
                 <Formik
                     initialValues={initialValues}
                     validationSchema={formValidation}
-                    onSubmit = { async (values) => {
-                        try {      
+                    onSubmit={async (values, { resetForm }) => {
+                        try {
+                            setProcessing(true)
                             const componentData = {
                                 ...values,
                                 author: session.user.name,
@@ -38,9 +59,14 @@ const ComponentForm = ({ session }) => {
                             // console.log(componentData)
                             const docRef = await addDoc(collection(db, "library"), componentData)
                             // console.log("Document written with ID: ", docRef.id);
+                            setProcessing(false)
+                            resetForm()
+                            handleOpen()
                         } catch (error) {
                             console.error("An error occured", error)
                             alert("Something went wrong! Try again later.")
+                        } finally {
+                            setProcessing(false)
                         }
 
                     }}
@@ -59,12 +85,31 @@ const ComponentForm = ({ session }) => {
                             <ErrorMessage name='component' component={"p"} className='text-red-600 text-xs' />
                         </div>
 
-                        <Button variant='outlined' type='submit' className='w-full mt-3'>
+                        <Button disabled={processing} variant='outlined' type='submit' className='w-full mt-3'>
                             Add your component
+                            {
+                                processing ? <TbLoader3 className='animate-spin text-2xl ml-3 text-gray-600' /> : null
+                            }
                         </Button>
                     </Form>
                 </Formik>
             </section>
+
+            <Modal
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
+            >
+                <Box sx={style}>
+                    <Typography id="modal-modal-title" variant="h6" component="h2">
+                        Sucessful Submission
+                    </Typography>
+                    <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                        Your component has been received and added to our growing collection
+                    </Typography>
+                </Box>
+            </Modal>
         </main>
     )
 }

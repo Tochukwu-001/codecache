87f22808bd67a494417fc40debbfddb72ef7f336
Tochukwu Ozo-Osiedo <tochukwuosiedo001@gmail.com>
2025-12-18 15:52:59 +0100

    updates

--- app/components/page.jsx
+++ app/components/page.jsx
@@ -1,74 +1,18 @@
-"use client";
-import React, { useEffect, useState } from 'react'
-import { FaTrash } from "react-icons/fa6";
-import { IoCopyOutline } from "react-icons/io5";
-import { collection, getDocs } from "firebase/firestore";
-import { db } from '@/config/firebase';
+import { auth } from '@/auth'
+import ComponentDisplay from '@/components/ComponentDisplay'
+import { redirect } from 'next/navigation'
+import React from 'react'
 
-const page = () => {
-
-    const [components, setComponents] = useState([])
-
-    const handleFetch = async () => {
-        const componentArray = []
-        const querySnapshot = await getDocs(collection(db, "library"));
-        querySnapshot.forEach((doc) => {
-            // doc.data() is never undefined for query doc snapshots
-            // console.log(doc.id, " => ", doc.data());
-            const componentObject = {
-                id: doc.id,
-                ...doc.data()
-            }
-            componentArray.push(componentObject)
-        });
-        setComponents(componentArray)
+const page = async () => {
+    const session = await auth()
+    if (!session) {
+        redirect("/auth/signin")
     }
-
-    useEffect(() => { handleFetch() }, [])
-
-
-    return (
-        <main className="min-h-dvh">
-            <section className="bg-[url('/bg.png')] h-[20vh]">
-                <div className='h-[20vh] bg-black/50 flex items-center justify-center p-3'>
-                    <h1 className='text-white text-center md:text-2xl text-lg font-semibold'>Explore our catalogue of Components from diverse Developers</h1>
-                </div>
-            </section>
-
-            <section className='grid lg:grid-cols-3 md:grid-cols-2 md:p-10 p-3 gap-5'>
-                {
-                    components.map((data, index) => (
-                        <div key={index} className='shadow-md p-4 rounded-md'>
-                            <div className='flex items-center justify-between'>
-                                <span className='flex items-center gap-2'>
-                                    <img src={data.img} alt={data.author} className='w-8 h-8 rounded-full' />
-                                    <p>{data.author}</p>
-                                </span>
-                                <button>
-                                    <FaTrash className='text-red-600' />
-                                </button>
-                            </div>
-
-                            <div>
-                                <h1 className='text-center my-4 font-semibold uppercase'>{data.name}</h1>
-                                <p className='line-clamp-5 text-sm mb-4'>
-                                    {data.component}
-                                </p>
-                            </div>
-
-                            <div className='flex items-center justify-between'>
-                                <p className='text-sm text-gray-600'>{data.timestamp}</p>
-                                <span className='flex items-center gap-1 text-sm'>
-                                    <IoCopyOutline />
-                                    <p>Copy Code</p>
-                                </span>
-                            </div>
-                        </div>
-                    ))
-                }
-            </section>
-        </main>
-    )
+  return (
+    <main>
+      <ComponentDisplay session={session}/>
+    </main>
+  )
 }
 
 export default page
